14a01a5bca95c187b3d8542e3edde847b8bf76cf
Xylo <itzxyloyaknow@gmail.com>
2024-07-11 18:45:21 -0700

    Create Calculator.java

---
+++ java/singlefile/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Basic Calculator!");
+        System.out.println("Choose an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter the number of the operation you want to perform: ");
+        int choice = scanner.nextInt();
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+        boolean validOperation = true;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    validOperation = false;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation choice.");
+                validOperation = false;
+        }
+
+        if (validOperation) {
+            System.out.println("The result is: " + result);
+        }
+
+        scanner.close();
+    }
+}
